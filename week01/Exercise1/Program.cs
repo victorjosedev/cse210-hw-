6c21da231c7376c9f236b25c468dffab11472b1d
Victor <vhenriquez@byupathway.edu>
2026-07-09 17:08:17 -0400

    Exercise 2 done!

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -3,12 +3,14 @@ using System;
 class Program
 {
     static void Main(string[] args)
-
     {
+        // Ask the user for their first and last name.
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
+
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
+
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }  
 }
